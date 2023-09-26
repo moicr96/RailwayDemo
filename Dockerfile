871e5c23c1d9ad91ff5d5189b07c8de58d81f63c
moicr96 <127130892+moicr96@users.noreply.github.com>
2023-09-25 22:20:04 -0600

    ede

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM openjdk:17
-ADD ./docker-spring-boot.war docker-spring-boot.war
-ENTRYPOINT ["java", "docker-spring-boot.war"]
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+/*
 	public String PORT = System.getenv("PORT");
+*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
